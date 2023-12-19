6e962a58d3482356658facbc8d83ad3c97b42d62
habibur18 <amarhabibur@gmail.com>
2023-12-19 13:46:21 +0600

    unistall chat bot and replace new one

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,3 @@
-import MessengerCustomerChat from "react-messenger-customer-chat";
 import Techstack from "../Sections/3_Techstack";
 import Projects from "../Sections/4_Projects";
 import AboutMe from "../Sections/6_AboutMe";
@@ -9,7 +8,6 @@ import Navbar from "../Sections/Navbar";
 const Home = () => {
   return (
     <div className="max-w-[1400px] mx-auto">
-      <MessengerCustomerChat pageId="209067758947640" appId="894693308537157" />
       <Navbar />
       <Intro />
       <Techstack />
