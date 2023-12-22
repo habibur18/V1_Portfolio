7a28a94deed108fe6a575b0e4bded1cc4efa6f6d
habibur18 <amarhabibur@gmail.com>
2023-12-22 21:53:36 +0600

    added updated resume

--- src/components/Sections/6_AboutMe.jsx
+++ src/components/Sections/6_AboutMe.jsx
@@ -41,7 +41,7 @@ const AboutMe = () => {
             <span className="text-lg font-semibold">Download Resume</span>
           </a>
           <a
-            href="https://www.docdroid.net/1bGS5X0/habibur-rahman-pdf"
+            href="https://drive.google.com/file/d/1hIv7tGLTPZkX7mtXF90x12U_7G8XvAod/view"
             target="_blank"
             rel="noopener noreferrer"
             className="bg-green-800 text-white px-4 py-2 rounded-full flex items-center space-x-2 transition-all duration-300 transform hover:scale-105"
